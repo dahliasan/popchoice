14bfa870b5b1140a9c34817f0e581096b8798465
dahliasan <laninaesloco@gmail.com>
2024-01-30 22:31:01 +1100

    ♻️ Refactor page layout and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,15 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import PopcornImage from '@/public/popchoice-icon.png'
-import Image from 'next/image'
+import Hero from '@/components/Hero'
+import ScreenOne from '@/components/ScreenOne'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-indigo-950'>
-      <div className='max-w-prose grid justify-items-center gap-4'>
-        <div className='flex flex-col items-center gap-2'>
-          <Image src={PopcornImage} alt='Popcorn' />
-          <h1 className='text-6xl font-display text-white text-center'>
-            Popchoice
-          </h1>
-        </div>
+    <main className='flex min-h-screen flex-col items-center pt-8 bg-indigo-950'>
+      <div className='max-w-prose min-w-[min(100%,250px)] grid justify-items-center gap-4'>
+        <Hero>
+          <h1 className='text-center'>Popchoice</h1>
+        </Hero>
 
-        <div className='grid w-full max-w-sm items-center gap-2'>
-          <div>
-            <Label htmlFor='pax'>How many people?</Label>
-            <Input id='pax' placeholder='5' />
-          </div>
-
-          <div>
-            <Label htmlFor='time'>How much time do you have?</Label>
-            <Input id='time' placeholder='2h 15min' />
-          </div>
-
-          <Button>Start!</Button>
-        </div>
+        <ScreenOne />
       </div>
     </main>
   )
