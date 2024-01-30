b7f8d3cc8247aca1cf486cddc3b8e77de3f01973
dahliasan <laninaesloco@gmail.com>
2024-01-30 22:31:19 +1100

    💄 Refactor input component styles

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -11,8 +11,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border-none border-input bg-background/20 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          'placeholder:text-indigo-950 focus-visible:ring-offset-0',
+          'flex h-10 w-full rounded-md border-none border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'placeholder:text-indigo-950 focus-visible:ring-off bg-indigo-300/50',
           className
         )}
         ref={ref}
