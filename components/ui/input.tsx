9793e79fce4901cdafd16712deba04e5a5df8893
dahliasan <laninaesloco@gmail.com>
2024-01-30 21:13:03 +1100

    💄 Update input placeholder styling and focus ring

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         type={type}
         className={cn(
           'flex h-10 w-full rounded-md border-none border-input bg-background/20 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          'placeholder:text-indigo-950',
+          'placeholder:text-indigo-950 focus-visible:ring-offset-0',
           className
         )}
         ref={ref}
