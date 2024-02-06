53f4971ade1e53ddcc9ee47067cd141667a9503b
dahliasan <laninaesloco@gmail.com>
2024-02-06 22:06:36 +1100

    🔧 Add tmdb as source to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.tmdb.org',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
