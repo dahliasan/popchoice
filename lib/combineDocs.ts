03760ae83272bdd9d5c0aca1a04c2a1f92efec35
dahliasan <laninaesloco@gmail.com>
2024-02-07 05:38:03 +1100

    ♻️ Add MovieMetadata to combineDocs function

--- lib/combineDocs.ts
+++ lib/combineDocs.ts
@@ -1,7 +1,11 @@
 import { formatDocumentsAsString } from 'langchain/util/document'
 import { retriever } from './config'
+import { MovieMetadata } from '@/types/types'
 
 export const combineDocs = async (input: string) => {
   const relevantDocs = await retriever.getRelevantDocuments(input)
-  return formatDocumentsAsString(relevantDocs)
+  return {
+    docs: relevantDocs as { pageContent: string; metadata: MovieMetadata }[],
+    docsAsString: formatDocumentsAsString(relevantDocs),
+  }
 }
