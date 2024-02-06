ac3b3664c43692639269301f4db202e22ca190b3
dahliasan <laninaesloco@gmail.com>
2024-02-07 07:05:06 +1100

    💄 Update image dimensions in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Hero({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex flex-col items-center gap-2'>
-      <Image src={PopcornImage} alt='Popcorn' />
+      <Image src={PopcornImage} alt='Popcorn' width={100} height={100} />
       {children}
     </div>
   )
