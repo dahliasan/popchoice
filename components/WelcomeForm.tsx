a39b898dfa74259160846a937936a54c588e5ad0
dahliasan <laninaesloco@gmail.com>
2024-02-07 21:35:58 +1100

    ♻️ Refactor WelcomeForm component and add conditional rendering for previous movie recommendations

--- components/WelcomeForm.tsx
+++ components/WelcomeForm.tsx
@@ -5,15 +5,17 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 import { useState } from 'react'
-import { number } from 'zod'
+import { useCompletionStore } from '@/lib/store'
 
 export default function WelcomeForm() {
   const [numberOfPeople, setNumberOfPeople] = useState('1')
+  const results = useCompletionStore((state) => state.results)
+
   return (
     <>
-      <div className='grid w-full max-w-sm items-center gap-2'>
+      <div className='grid w-full max-w-sm items-center gap-2 sapce-y-2'>
         <div>
-          <Label htmlFor='pax'>How many people?</Label>
+          <Label htmlFor='pax'>How many people are watching the movie?</Label>
           <Input
             id='pax'
             placeholder='5'
@@ -30,6 +32,12 @@ export default function WelcomeForm() {
         <Link href={`/start?numberOfPeople=${numberOfPeople}`}>
           <Button className='w-full'>Start!</Button>
         </Link>
+
+        {results.length > 0 && (
+          <Link href='/results'>
+            Been here before? See previous movies recommended for you
+          </Link>
+        )}
       </div>
     </>
   )
