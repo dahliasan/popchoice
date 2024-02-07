70c0ba0c64f02da03902195d84111f3232fba390
dahliasan <laninaesloco@gmail.com>
2024-02-07 21:34:31 +1100

    ♻️ Update Home page layout and content

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,15 @@ import WelcomeForm from '@/components/WelcomeForm'
 
 export default function Home() {
   return (
-    <main className='min-h-screen grid place-content-center py-8'>
+    <main className='min-h-screen grid place-content-center py-8 px-4'>
       <div className='max-w-prose min-w-[min(100%,250px)] grid justify-items-center gap-4'>
         <Hero>
-          <h1 className='text-center'>Popchoice</h1>
+          <div className='max-w-sm text-center text-balance space-y-2'>
+            <h1 className='text-center'>Popchoice</h1>
+            <p className='font-display text-sm'>
+              Stop wasting time deciding what to watch. Popchoice will help you.
+            </p>
+          </div>
         </Hero>
 
         <WelcomeForm />
